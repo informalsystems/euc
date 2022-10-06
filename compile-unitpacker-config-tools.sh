91bc151ab23a05a5e9c7426340b3d225585290a0
Harveen Singh <harveen.s@hotmail.com>
2022-10-06 16:15:40 -0400

    Added multisig and related files

---
+++ compile-unitpacker-config-tools.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# GitHub Actions script to gather configuration from the local config.json file
+set -eux
+
+# Find tool configuration from config.json - Output: TOOL_CONFIG 
+TOOL_NAME="$(echo "$1" | sed 's/^\(.*\)-v.*$/\1/')"
+TOOL_CONFIG="$(jq '.tools | map(select(.name == "'"$TOOL_NAME"'"))[0]' config.json)"
+
+# Get the 'os' and 'tool' for unitpacker.
+CONFIG="$(jq -s '{os: .[0].os} + {tools: .[0].tools}' config.json | tr -d '\n')"
+echo "::set-output name=config::$CONFIG"
