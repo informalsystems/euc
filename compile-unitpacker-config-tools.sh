760ec9753e76daec43984113788946af9a493e7c
Harveen Singh <harveen.s@hotmail.com>
2022-11-07 15:24:38 -0500

    Made changes suggested in the PR

--- compile-unitpacker-config-tools.sh
+++ compile-unitpacker-config-tools.sh
@@ -7,5 +7,5 @@ TOOL_NAME="$(echo "$1" | sed 's/^\(.*\)-v.*$/\1/')"
 TOOL_CONFIG="$(jq '.tools | map(select(.name == "'"$TOOL_NAME"'"))[0]' config.json)"
 
 # Get the 'os' and 'tool' for unitpacker.
-CONFIG="$(jq -s '{os: .[0].os} + {tools: .[0].tools}' config.json "${TOOL_NAME}_config.json" | tr -d '\n')"
+CONFIG="$(jq -s '{os: .[0].os} + {tools: .[0].tools}' config.json | tr -d '\n')"
 echo "::set-output name=config::$CONFIG"
