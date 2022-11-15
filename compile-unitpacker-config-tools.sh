e56113c32fd7fc07c43fa37993cb2b3c43136474
Harveen Singh <harveen.s@hotmail.com>
2022-11-15 00:04:53 -0500

    Made changes to unitpacker bash

--- compile-unitpacker-config-tools.sh
+++ compile-unitpacker-config-tools.sh
@@ -5,7 +5,8 @@ set -eux
 # Find tool configuration from config.json - Output: TOOL_CONFIG 
 TOOL_NAME="$(echo "$1" | sed 's/^\(.*\)-v.*$/\1/')"
 TOOL_CONFIG="$(jq '.tools | map(select(.name == "'"$TOOL_NAME"'"))[0]' config.json)"
+echo "$TOOL_CONFIG" > "${TOOL_NAME}_config.json"
 
 # Get the 'os' and 'tool' for unitpacker.
-CONFIG="$(jq -s '{os: .[0].os} + {tools: .[0].tools}' config.json | tr -d '\n')"
+CONFIG="$(jq -s '{os: .[0].os} + {tools: [.[1]]}' config.json "${TOOL_NAME}_config.json" | tr -d '\n')"
 echo "::set-output name=config::$CONFIG"
